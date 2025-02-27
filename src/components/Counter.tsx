88efe2714fe1067faa55c53b0dfe290a6800dc68
Chad Taylor <chadtaylor@hey.com>
2025-02-26 21:20:13 -0600

    feat: add vitest setup

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button } from "antd";
+// import Button from "antd/lib/button";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    maxConcurrency: 20,
+    pool: "threads",
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
+    deps: {
+      optimizer: {
+        web: {
+          enabled: true,
+        },
+      },
+    },
+  },
+});
